c5d876253d11c693e247b73a1a46c75bb2844df4
AFinalStone <602392033@qq.com>
2016-06-16 15:47:13 +0800

    修改包名

--- app/src/main/java/com/shi/androidstudy/swiperefreshview_master/MainActivity.java
+++ app/src/main/java/com/shi/androidstudy/swiperefreshview_master/MainActivity.java
@@ -3,11 +3,15 @@ package com.shi.androidstudy.swiperefreshview_master;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * MainActivity
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
